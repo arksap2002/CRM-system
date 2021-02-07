bbdec3b4e8d48b4ce4028913018caae6700a77eb
arkady <arksap2002@gmail.com>
2021-02-07 07:03:59 +0300

    add vector clients

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
-#include <iostream>
+#include "client.h"
+#include <vector>
+#include <cassert>
 
 int main() {
-    std::cout << "Dobroe utro";
+    std::vector<client::Client> clients;
+    clients.emplace_back("Anna", "Popupanova");
+    clients.emplace_back("Denis", "Shectakov");
+    clients.emplace_back("Arkady", "Sapozhnikov");
+    assert(clients.size() == 3);
+    clients[0].setSurname("Lipatova");
+    assert(clients[0].getSurname() == "Lipatova");
 }
